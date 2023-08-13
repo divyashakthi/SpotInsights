39079f71eac92289f907b3cf6d0a55a4753a0fe3
DIVYA SHAKTHI R <rdivyashakthi@gmail.com>
2023-08-13 19:16:58 +0530

    Initial commit

--- spotinsights.py
+++ spotinsights.py
@@ -3,42 +3,52 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from flask import Flask, render_template, request
+import re
 
-username='l83z3ki3m59kg3gwigkq1n9hu'
-client_id='ac60f560de064304ad3cefba50a23cf0'
-client_secret='9dce9377d9244127884f22dc8ae1757e'
-redirect='https://www.spotify.com/'
-oauth_object=spotipy.SpotifyOAuth(client_id,client_secret,redirect)
-client_credentials_manager=SpotifyClientCredentials(client_id,client_secret)
-sp=spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+app = Flask(__name__)
 
-def get_tracks():
-    # music_list=[]
-    artist_list=[]
-    playlist=sp.playlist("7tZjUDmiIwSFl28aOZqKr0")
+client_id = 'ab49552708ba40b3ac524a77d47f8558'
+client_secret = 'dbff214ff84949bebfe907965df65440'
+client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def index():
+    if request.method == 'POST':
+        playlist_url = request.form['playlist_url']
+        pattern = r'playlist\/([a-zA-Z0-9]+)'
+        match = re.search(pattern, playlist_url)
+        if match:
+            playlist_id = match.group(1)
+            get_tracks(playlist_id)
+    return render_template('index.html')
+
+def get_tracks(playlist_id):
+    artist_list = []
+    playlist = sp.playlist(playlist_id)
     for item in playlist['tracks']['items']:
-        music_track=item['track']
+        music_track = item['track']
         artists = music_track['artists']
         artist_names = [artist['name'] for artist in artists]
-        artist_list.append(artist_names)
-        # music_list.append(music_track['name'])
-        # print(artist_list)
-    # return music_list
+        artist_list.extend(artist_names)
 
     artist_counts = pd.Series(artist_list).value_counts()
     colors = sns.color_palette("flare")
     plt.rcParams['font.family'] = 'sans-serif'
     plt.rcParams['font.size'] = 12
-    
+
     plt.figure(figsize=(10, 6), facecolor='#D8BFD8')
-    explode = (0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    explode = tuple(0.1 if i == 0 else 0 for i in range(len(artist_counts[:10])))
     pie = artist_counts[:10].plot(kind='pie', colors=colors, autopct='%1.1f%%', startangle=140, pctdistance=0.85, explode=explode)
     title_font = {'family': 'monospace', 'fontname': 'Consolas', 'size': 26, 'weight': 'bold'}
     pie.set_title('Top 10 Artists in Playlist', fontdict=title_font)
     pie.set_ylabel('')
     pie.legend(artist_counts[:10].index, loc='lower left', bbox_to_anchor=(1.2, 0.1), fontsize=10)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('static/div_insights.png')
+    plt.close()
 
-# print(get_artists())
-get_tracks()
+if __name__ == '__main__':
+    app.run(debug=True)
